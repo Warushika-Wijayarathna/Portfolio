55104636c4ed3b8f2589b03d2a9e9ffc8dc1bf9e
Warushika-Wijayarathna <warushikawijayarathna@gmail.com>
2024-10-25 23:08:20 +0530

    responsive designs

--- scripts/script.js
+++ scripts/script.js
@@ -19,4 +19,38 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
+const tiltSettings = {
+    max: 100,
+    perspective: 1500,
+    scale: 1.2
+};
+
+const cards = document.querySelectorAll(".project-card").forEach((card) => {
+    card.addEventListener("mousemove", (e) => mouseMove(e, card));
+    card.addEventListener("mouseleave", (e) => mouseLeave(e, card));
+});
+
+function mouseMove(event, card) {
+    const cardWidth = card.getBoundingClientRect().width;
+    const cardHeight = card.getBoundingClientRect().height;
+    const centerX = card.getBoundingClientRect().left + cardWidth / 2;
+    const centerY = card.getBoundingClientRect().top + cardHeight / 2;
+    const mouseX = event.clientX - centerX;
+    const mouseY = event.clientY - centerY;
+    const rotateX = (mouseY / cardHeight / 2) * tiltSettings.max;
+    const rotateY = (mouseX / cardWidth / 2) * tiltSettings.max * -1;
+
+    console.log(centerX, centerY);
+
+    card.style.transform = `perspective(${tiltSettings.perspective}px)
+                            rotatex(${rotateX}deg)
+                            rotatey(${rotateY}deg)
+                            scale3d(${tiltSettings.scale}, ${tiltSettings.scale}, ${tiltSettings.scale})`;
+}
+
+function mouseLeave(event, card) {
+    card.style.transform = "";
+}
+
+
 
